37ea146c283870b66a701306ba80968aea11f805
abdul452 <abdulsalimmkm@gmail.com>
2026-05-25 09:19:22 +0700

    selesai

--- 01-context_test/context_test.go
+++ 01-context_test/context_test.go
@@ -132,3 +132,20 @@ func TestContextWithTimeout(t *testing.T) {
 
 	fmt.Println("Total Goroutine", runtime.NumGoroutine())
 }
+
+func TestContextWithDeadline(t *testing.T) {
+	fmt.Println("Total Goroutine", runtime.NumGoroutine())
+
+	parent := context.Background()
+	ctx, cancel := context.WithDeadline(parent, time.Now().Add(5*time.Second))
+	defer cancel() // jika proses nya lebih cepat dari waktu deadline, maka cancel() akan langsung menghentikan go routine tanpa harus menunggu hinggal waktu deadline habis
+
+	destination := CreateCounter(ctx)
+	for n := range destination {
+		fmt.Println("Counter", n)
+	}
+
+	time.Sleep(2 * time.Second)
+
+	fmt.Println("Total Goroutine", runtime.NumGoroutine())
+}
